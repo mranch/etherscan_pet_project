6d42de683b6f5d8b4d2b084fa655bb2df616af11
mranch <anchiforov@gmail.com>
2022-03-16 22:31:23 +0200

    create display_data function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import webbrowser
 from datetime import datetime
 
 from etherscan import Etherscan
@@ -32,7 +33,7 @@ def parse_block(block_number):
     }
 
 
-def parse_transaction(transaction):
+def parse_transaction(transaction, balance):
     transaction_hash = transaction["hash"]
     transaction_date = datetime.utcfromtimestamp(int(transaction["timeStamp"])).strftime("%d.%m.%y")
     income_amount = convert_amount(transaction["value"])
@@ -53,18 +54,88 @@ def parse_transaction(transaction):
     }
 
 
-etherscan_client = initialize_etherscan_client()
-mongo_client_cluster, mongo_client_database, mongo_client_collection = initialize_mongo_client()
-my_address = config.ADDRESS
+def process_transactions(mongo_client_collection):
+    etherscan_client = initialize_etherscan_client()
+    my_address = config.ADDRESS
 
-balance = etherscan_client.get_eth_balance(my_address)
+    balance = etherscan_client.get_eth_balance(my_address)
 
-my_transactions = etherscan_client.get_normal_txs_by_address(
-    address=my_address,
-    startblock=0,
-    endblock=99999999,
-    sort="asc"
-)
+    my_transactions = etherscan_client.get_normal_txs_by_address(
+        address=my_address,
+        startblock=0,
+        endblock=99999999,
+        sort="asc"
+    )
 
-parse_transaction_results = [parse_transaction(transaction) for transaction in my_transactions]
-mongo_client_collection.insert_many(parse_transaction_results)
+    parse_transaction_results = [parse_transaction(transaction, balance) for transaction in my_transactions]
+    mongo_client_collection.insert_many(parse_transaction_results)
+
+
+def display_data(mongo_client_collection, open_in_browser=False):
+    filename = "output.html"
+    transaction_data_list = []
+    first_time = True
+    for transaction in mongo_client_collection.find():
+        if first_time:
+            first_time = False
+            transaction_headers = "\n".join(f"\t\t\t\t<th>{header}</th>" for header in transaction)
+            transaction_data_list.append(
+                f"""<tr>
+{transaction_headers}
+            </tr>
+            """
+            )
+        transaction_values = "\n".join(f"\t\t\t\t<td>{transaction[key]}</td>" for key in transaction)
+        transaction_data_list.append(
+            f"""<tr>
+{transaction_values}
+            </tr>
+            """
+        )
+    transactions = "".join(transaction_data_list).rstrip()
+
+    content = f"""<!DOCTYPE html>
+<html>
+    <head>
+        <meta content="text/html">
+        <title>My Transactions</title>
+        <style>
+            td, th {{
+                border: 1px solid #ddd;
+                padding: 8px;
+            }}
+            tr:nth-child(even){{background-color: #f2f2f2;}}
+            tr:hover {{background-color: #ddd;}}
+            th {{
+              padding-top: 12px;
+              padding-bottom: 12px;
+              text-align: left;
+              background-color: #04AA6D;
+              color: white;
+            }}
+        </style>
+    </head>
+    <body>
+        <table>
+            {transactions}
+        </table>
+    </body>
+</html>
+    """
+
+    with open(filename, "w") as out:
+        out.write(content)
+
+    if open_in_browser:
+        webbrowser.open(filename)
+
+
+def main(insert_transactions=False):
+    mongo_client_cluster, mongo_client_database, mongo_client_collection = initialize_mongo_client()
+    if insert_transactions:
+        process_transactions(mongo_client_collection)
+    display_data(mongo_client_collection, open_in_browser=True)
+
+
+if __name__ == "__main__":
+    main()
